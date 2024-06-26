2bac89777579235e71b960ada37857aa42ec6e6f
LPGSenoth <163125498+LPGSenoth@users.noreply.github.com>
2024-06-26 11:34:10 +0200

    qdsfq

--- Src/Gus_work.py
+++ Src/Gus_work.py
@@ -1,1 +1,7 @@
 fffffffffffffffffffffffffffdffqdqfqfdqdfdssqdfdfdqfdsqfd
+quote_from_bytes(POSIX_FADV_SEQUENTIALqff
+                 quitfsq
+                 fq
+                 f
+                 pack_fopaque(qfdsfq
+                              , data), safe='/')
